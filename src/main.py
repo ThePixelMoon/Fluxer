bbcfa4b08ab627c1a80526bd396f578104dc5f3a
ThePixelMoon <yourlocalmoon7@gmail.com>
2024-11-06 18:38:51 +0200

    test

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+import argparse as ap
 from manager import *
+LINK = "https://raw.githubusercontent.com/ThePixelMoon/Fluxer/refs/heads/main/" # the package manager files because yes
 
-LINK = "" # the package manager files because yes
+if __name__ == "__main__":
+    parser = ap.ArgumentParser(description="a free-to-use package manager")
+    parser.add_argument('package', type=str, help="the package to install")
+    parser.add_argument('--verbose', action='store_true', help="enable verbose output")
+    args = parser.parse_args()
+
+    mgr = Manager(LINK)
+    mgr.parse(args.package, args.verbose) # parse the package
--- src/manager.py
+++ src/manager.py
@@ -1,5 +1,33 @@
 # manager.py
+import urllib.request
+import sys, os, tarfile
+
+supports_tar_xz = False
+
+def progress_callback(block_num: int, block_size: int, total_size: int) -> None:
+    downloaded = block_num * block_size
+    if total_size > 0:
+        percent = downloaded * 100 / total_size
+        sys.stdout.write(f"\rdownloading.. {percent:.2f}%")
+        sys.stdout.flush()
 
 class Manager:
     def __init__(self, host: str) -> None:
-        ...
+        self.host = host
+
+    def parse(self, package: str, verbose: bool) -> None:
+        if not supports_tar_xz:
+            url = f"{host}packages/{package}.tar"
+        else:
+            url = f"{host}packages/{package}.tar.xz"
+        
+        os.makedirs("installed", exist_ok=True)
+        to = os.path.join("installed", f"{package}.tar.xz")
+        urllib.request.urlretrieve(url, to, reporthook=progress_callback) # download
+        if verbose:
+            print("downloaded")
+
+        with tarfile.open(to, "r:xz") as tar:
+            tar.extractall("installed")
+            if verbose:
+                print("extracted")
