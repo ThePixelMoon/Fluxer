fd376affbdf141a959291d6b99da7b767b3d0ceb
ThePixelMoon <yourlocalmoon7@gmail.com>
2024-11-07 13:49:11 +0200

    bre

--- src/main.py
+++ src/main.py
@@ -2,13 +2,18 @@
 import argparse as ap
 from manager import *
 
-LINK = "https://raw.githubusercontent.com/ThePixelMoon/Fluxer/refs/heads/main/" # the package manager files because yes
+LINK = "https://raw.githubusercontent.com/ThePixelMoon/Fluxer/refs/heads/main/"  # the package manager files
 
 if __name__ == "__main__":
     parser = ap.ArgumentParser(description="a free-to-use package manager")
-    parser.add_argument('package', type=str, help="the package to install")
+    parser.add_argument('package', type=str, help="the package to install or search for")
+    parser.add_argument('--find', action='store_true', help="search for a package")
     parser.add_argument('--verbose', action='store_true', help="enable verbose output")
+    
     args = parser.parse_args()
-
     mgr = Manager(LINK)
-    mgr.parse(args.package, args.verbose) # parse the package
+
+    if args.find:
+        mgr.find_package(args.package)
+    else:
+        mgr.parse(args.package, args.verbose)
